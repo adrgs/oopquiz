eeff07835aa9f7ead6aced5aab08ec01b1df0dfe
adrgs <albastroiudragos@gmail.com>
2020-01-30 19:34:15 +0200

    Small changes

--- surse/prob38.cpp
+++ surse/prob38.cpp
@@ -4,7 +4,6 @@ using namespace std;
 template<class X, class Y>
 X f(X x, Y y)
 {
-    //aici o sa ajunga niste pointer si nu-i poti aduna!!!
     return x+y;
 }
 int *f(int *x,int y)
@@ -14,7 +13,6 @@ int *f(int *x,int y)
 int main()
 {
     int *a=new int(200), *b=a;
-    //cout<<a+b<<"\n"; nu poti aduna pointeri
     cout<<*f(a,b);
     return 0;
 }
