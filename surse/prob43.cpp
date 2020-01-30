94cda14e3b89674cea55a378e075ccea963a35fb
adrgs <albastroiudragos@gmail.com>
2020-01-30 19:33:23 +0200

    Mici modificari

--- surse/prob43.cpp
+++ surse/prob43.cpp
@@ -5,26 +5,25 @@ class cls
 {
     int x;
 public:
-        cls(int i=0) {cout<<" c1 "; x=i;}
-        ~cls() {cout<<" d 1 ";}
+        cls(int i=0) {cout<<"c1 "; x=i;}
+        ~cls() {cout<<"d1 ";}
 };
 class cls1
 {
     int x; cls xx;
 public:
-        cls1(int i=0){cout<<" c2 ";x=i;}
-        ~cls1(){cout<<" d2 ";}
+        cls1(int i=0){cout<<"c2 ";x=i;}
+        ~cls1(){cout<<"d2 ";}
 }c;
 class cls2
 {
     int x;cls1 xx;cls xxx;
 public:
-    cls2(int i=0) {cout<<" c3 ";x=i;}
-    ~cls2(){ cout<<" d3 ";}
+    cls2(int i=0) {cout<<"c3 ";x=i;}
+    ~cls2(){ cout<<"d3 ";}
 };
 int main()
 {
     cls2 s;
     return 0;
 }
-//c1 c2 c1 c2 c1 c3 d3 d1 d2 d1 d2 d1
